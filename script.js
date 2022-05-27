b456656890c43dbd2aad04ac3f2df3f61f3df3e5
nanarama <annasampers@gmail.com>
2022-05-27 15:37:43 -0500

    Add limit of characters to display, add if statements to check for long numbers and not needed decimals

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const operator = document.querySelectorAll('.operator');
 const display = document.querySelector('.display');
 const clear = document.querySelector('.clear');
 const equals = document.querySelector('.equals');
-const dot = document.querySelector('.operand.dot');
+const dot = document.querySelector('.operand.dot')
+display.length = 0;
+dot.disabled = false;
+let decimal = '.';
 let isNum = false;
 let isOpr = false;
 let isNum2 = false;
@@ -36,22 +39,31 @@ const operate = function(num, opr, num2) {
 };
 
 operand.forEach(btn => btn.addEventListener('click', (e) => { 
-    if (isOpr === true || isAnswer === true) {
-        input.num2 = e.target.value;
-        displayValues.num2 += input.num2
-        display.textContent = displayValues.num2;
-        isNum2 = true;
-    } else {
-        input.num = e.target.value;
-        displayValues.num += input.num
-        display.textContent = displayValues.num;
-        isNum = true;
-    }
-}));
+    if(display.length < 10 || display.length == undefined) {
+        if (isOpr === true || isAnswer === true) {
+            input.num2 = e.target.value;
+            displayValues.num2 += input.num2
+            display.textContent = displayValues.num2;
+            display.length++;
+            isNum2 = true;
+            operator.forEach(btn => btn.disabled = false);
+        } else {
+            input.num = e.target.value;
+            displayValues.num += input.num
+            display.textContent = displayValues.num;
+            display.length++;
+            isNum = true;
+        }
+}}));
+
+dot.addEventListener('click', function() {
+    dot.disabled = true;
+})
 
 operator.forEach(btn => btn.addEventListener('click', (e) => {    
     if(isNum2 === true) {
         getAnswer();
+        dot.disabled = false;
     }
     if(e.target.id.toString() === 'divide') {
         displayValues.opr = divide;
@@ -63,7 +75,10 @@ operator.forEach(btn => btn.addEventListener('click', (e) => {
         displayValues.opr = add;
     }
     isOpr = true;
+    dot.disabled = false;
+    display.length = 0;
     display.textContent += e.target.value;
+    operator.forEach(btn => btn.disabled = true);
     
 }));
 
@@ -75,6 +90,9 @@ equals.addEventListener('click', function() {
 
 clear.addEventListener('click', function() {
     display.textContent = '';
+    display.length = 0;
+    dot.disabled = false;
+    operator.forEach(btn => btn.disabled = false);
     isNum = false;
     isOpr = false;
     isNum2 = false;
@@ -86,16 +104,22 @@ clear.addEventListener('click', function() {
         answer: '',
     };
 });
-//ability to continue adding or subtracting numbers to the answer, not dependent on equals
-//function to get answer
+
 function getAnswer() {
-    displayValues.answer = operate(displayValues.num, displayValues.opr, displayValues.num2);
+    displayValues.answer = Number(operate(displayValues.num, displayValues.opr, displayValues.num2)).toFixed(2);  
+    if(`${displayValues['answer']}`.length > 11) {
+        displayValues.answer = Number(displayValues['answer']).toExponential(4);
+    } 
+    if(`${displayValues['answer']}`.includes('.00')) {
+        displayValues.answer = `${displayValues['answer']}`.slice(0, -3);
+    }  
     display.textContent = displayValues.answer;
+    operator.forEach(btn => btn.disabled = false);
     displayValues.opr = '';
     displayValues.num2 = '';
     displayValues.num = displayValues.answer;
+    display.length = 0;
     isOpr = false;
     isNum2 = false;
     isAnswer = true;
 };
-//function to take answer, add operator, take num2 and get new answer
